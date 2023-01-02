15cb6f2a2b15a92ad0eb92f8e12bf9b01c7ae8cc
Erik Golinelli <erik@codekraft.it>
2023-01-02 12:50:03 +0100

    now the function that generates the colorset creates it exactly with the name of the json (however in ts format)

--- colorSetUtils.mjs
+++ colorSetUtils.mjs
@@ -19,27 +19,57 @@ export function buildColorSet (colorSet) {
   return colors
 }
 
-function processSet (newSet, filename) {
+/**
+ *
+ * @param newSet
+ * @param filename
+ *
+ * @return {boolean} true whenever the file is successfully written
+ */
+export function processSet (newSet, filename) {
   const generated = buildColorSet(newSet)
-  const setName = `const colorSet: RGBCOLORDEF[] = `
+  const setName = `const ${filename}: RGBCOLORDEF[] = `
   const exportName = `
 
-export default ${filename}ColorSet`
+export default ${filename}`
 
   const generatedJson = setName + JSON.stringify(generated, null, 2) + exportName
 
-  fs.writeFile('./src/data/' + filename + 'ColorSet.ts', generatedJson, (err, data) => {
+  console.log('writing into ./src/data/' + filename + '.ts')
+
+  fs.writeFile('./src/data/' + filename + '.ts', generatedJson, (err, data) => {
     if (err) {
-      return console.log(err)
+      console.log(err)
+      return false
     }
     console.log(data)
+    return true
   })
+}
+
+/**
+ * This function will read the data from a json file then output it into a ts file that contains the colorSet data into a compatible format with this module
+ *
+ * @param {*} filename
+ * @param {string} fileUri
+ */
+export function generateColorSet(filename, fileUri) {
+  console.log('Reading ' + fileUri)
+  const set = fs.readFileSync(fileUri, 'utf8').replace(/\r\n/g, '\n').toString()
+
+  /** the json with the colorSet */
+  const jsonSet = JSON.parse(set)
+
+  return processSet(jsonSet, filename)
 }
 
 const filename = process.argv[2].replace(/[^a-z0-9]/gi, '')
 const fileUri = './src/data/' + filename + '.json'
-console.log('Reading ' + fileUri)
-const set = fs.readFileSync(fileUri, 'utf8').replace(/\r\n/g, '\n').toString()
-const jsonSet = JSON.parse(set)
-console.log(jsonSet)
-processSet(jsonSet, filename)
+
+/** Create the colorset then output the result */
+if ( generateColorSet(filename, fileUri) ) {
+  console.log('😎 Successfully created colors set ./src/data/' + filename + '.ts')
+} else {
+  console.log( '😓 Something went wrong! cannot create colors set ./src/data/' + filename + '.ts')
+}
+process.exit(0)
