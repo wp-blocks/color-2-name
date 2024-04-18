50ee06e0b41ad410316bac896a65e0199b7459a6
Erik Golinelli <erik@codekraft.it>
2024-04-18 21:54:23 +0200

    test updated

--- tests/colors_rgb.test.ts
+++ tests/colors_rgb.test.ts
@@ -1,44 +1,67 @@
 /**
  * COMMON FUNCTIONS TESTING:
  */
+import { describe, expect, it, vi } from "vitest";
+import { parseColor } from "../src";
+import { RGB, getRgbValues, parseRgb } from "../src/rgb-utils";
+import { normalizeRGB } from "./fixtures/functions";
+import { rgb_invalid_tests, rgb_valid_tests } from "./fixtures/rgb_colors";
 
-import {getRgbValues, parseRgb, RGB} from "../src/rgb-utils";
-import {rgb_invalid_tests, rgb_valid_tests} from "./fixtures/rgb_colors";
-import {normalizeRGB} from "./fixtures/functions";
-import {parseColor} from "../src";
-
-describe('RGB COMMON', () => {
-
-  // RGB
-  it('Returns the correct rgb representation of the given RGB color', () => {
-    expect(parseColor('rgb(255,255,255)')).toMatchObject({r: 255, g: 255, b: 255})
-    expect(parseColor('rgb(255, 255, 255,.5)')).toMatchObject({r: 255, g: 255, b: 255})
-    expect(parseColor('rgb(255 255 255)')).toMatchObject({r: 255, g: 255, b: 255})
-    expect(parseColor('rgb(255 255 255 / .5)')).toMatchObject({r: 255, g: 255, b: 255})
-    expect(parseColor('rgba(255 255 255 / 0.1)')).toMatchObject({r: 255, g: 255, b: 255})
-  })
-})
+describe("RGB COMMON", () => {
+	// RGB
+	it("Returns the correct rgb representation of the given RGB color", () => {
+		expect(parseColor("rgb(255,255,255)")).toMatchObject({
+			r: 255,
+			g: 255,
+			b: 255,
+		});
+		expect(parseColor("rgb(255, 255, 255,.5)")).toMatchObject({
+			r: 255,
+			g: 255,
+			b: 255,
+		});
+		expect(parseColor("rgb(255 255 255)")).toMatchObject({
+			r: 255,
+			g: 255,
+			b: 255,
+		});
+		expect(parseColor("rgb(255 255 255 / .5)")).toMatchObject({
+			r: 255,
+			g: 255,
+			b: 255,
+		});
+		expect(parseColor("rgba(255 255 255 / 0.1)")).toMatchObject({
+			r: 255,
+			g: 255,
+			b: 255,
+		});
+	});
+});
 
 /**
  * RGB FUNCTIONS TESTING:
  */
-describe('RGB', () => {
+describe("RGB", () => {
+	describe("RGB Color Parsing and Conversion", () => {
+		for (const [rgbString, expectedRgbString, description] of rgb_valid_tests) {
+			it(
+				description || `Parses RGB: ${rgbString} to RGB: ${expectedRgbString}`,
+				() => {
+					expect(RGB(getRgbValues(parseRgb(rgbString)))).toBe(
+						normalizeRGB(expectedRgbString),
+					);
+				},
+			);
+		}
+	});
 
-  describe('RGB Color Parsing and Conversion', () => {
-    rgb_valid_tests.forEach(([rgbString, expectedRgbString, description]) => {
-      it(description || `Parses RGB: ${rgbString} to RGB: ${expectedRgbString}`, () => {
-        expect(RGB(getRgbValues(parseRgb(rgbString)))).toBe(normalizeRGB(expectedRgbString));
-      });
-    });
-  });
-
-  describe('Invalid RGB Color Parsing', () => {
-    console.warn = jest.fn();
-    rgb_invalid_tests.forEach(([rgbString, expectedErrorMessage]) => {
-      it(`Fails to Parse Invalid RGB: ${rgbString} ${expectedErrorMessage}`, () => {
-        RGB(getRgbValues(parseRgb(rgbString)))
-        expect(console.warn).toBeCalled();
-      });
-    });
-  });
-})
+	describe("Invalid RGB Color Parsing", () => {
+		console.warn = vi.fn();
+		for (const [rgbString, expectedErrorMessage] of rgb_invalid_tests) {
+			it(`Fails to Parse Invalid RGB: ${rgbString} ${expectedErrorMessage}`, () => {
+				RGB(getRgbValues(parseRgb(rgbString)));
+				expect(console.warn).toBeCalled();
+			});
+		}
+	});
+});
