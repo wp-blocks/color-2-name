68d07bf196a9b9bddced5dc6a19d8b09e2ffb6e3
Erik Golinelli <erik@codekraft.it>
2025-11-27 19:33:15 +0100

    vitest and biome updated configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,12 @@
 /// <reference types="vitest" />
-import { defineConfig } from "vite";
+import { defineConfig } from "vitest/config";
 
 export default defineConfig({
 	test: {
 		coverage: {
 			reporter: ["text", "json", "html"],
 			exclude: ["**/node_modules/**", "**/lib/**"],
-			include: ["src/**"],
-			extension: ["ts", "tsx"],
+			include: ["src/**/*.[ts,tsx]"],
 		},
 	},
 });
